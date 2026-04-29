a934a855016f372edcf5c3b8d1bd81f841cf9f99
Anti9to5club <johntapiarea@gmail.com>
2026-04-28 23:59:44 -0700

    Fix Vercel build output

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: ".next-webpack",
   turbopack: {
     root: path.join(__dirname)
   }
